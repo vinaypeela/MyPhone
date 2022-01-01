e280822137f5d815fbfe0498f4095da922c07e4f
vinaypeela <vinay.peela16@gmail.com>
2022-01-01 10:40:07 +0530

    Phone Name Functonlity

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -1,29 +1,12 @@
 package org.phone;
 
 public class PhoneInfo {
-public void phoneName() {
+public static void phoneName() {
 System.out.println("phone name realme");
 }
-public void phoneEmiNumber() {
-System.out.println("Phone emi number 311129520036");
-}
-public void camera() {
-System.out.println("phone camer 64 mega pixeal");
-}
-public void storage() {
-System.out.println("storage 128 GB");
-}
-public void osName() {
-System.out.println("os name colouros v11");
-}
 public static void main(String[] args) {
-	PhoneInfo my = new PhoneInfo ();
+phoneName();
 	
-	my.phoneName();
-	my.phoneEmiNumber();
-	my.camera();
-	my.storage();
-	my.osName();
 	
 }
 }

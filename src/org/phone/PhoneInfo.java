49e7ef181d0b0d4150bbc0bff055e3b5f16db946
vinaypeela <vinay.peela16@gmail.com>
2022-01-01 10:47:18 +0530

    Phone EMI Functionality

--- src/org/phone/PhoneInfo.java
+++ src/org/phone/PhoneInfo.java
@@ -4,7 +4,6 @@ public class PhoneInfo {
 public static void phoneEmiNumber() {
 System.out.println("Phone emi number 311129520036");
 }
-
 public static void main(String[] args) {
 phoneEmiNumber();
 
